980342986d0cf06c507794d1c4779f38a4421ff4
ThanKarab <tkarabatsis@hotmail.com>
2019-05-30 15:38:41 +0300

    New heatmap highchart response

--- tools/exaremeTools/HEATMAP_HIGHCHART/HEATMAP_HIGHCHART.py
+++ tools/exaremeTools/HEATMAP_HIGHCHART/HEATMAP_HIGHCHART.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import json
 import sys
 
@@ -11,10 +13,16 @@ def getopts(argv):
         else:
             argv = argv[1:]
     return opts
-	
-def heatmap(inputJson, title, xtitle, ytitle):
 
-    #https://www.highcharts.com/demo/heatmap
+
+def heatmap(
+    inputJson,
+    title,
+    xtitle,
+    ytitle,
+    ):
+
+    # https://www.highcharts.com/demo/heatmap
 
     xcategories = []
     ycategories = []
@@ -22,73 +30,102 @@ def heatmap(inputJson, title, xtitle, ytitle):
 
     init = True
     for x in inputJson:
+
         # print 'x',x
+
         if init is True:
-            variablenames =[]
-            for key,value in x.iteritems():
+            variablenames = []
+            for (key, value) in x.iteritems():
                 variablenames.append(str(key))
                 init = False
+
             # print 'variablenames:', str(variablenames)
         # print 'inputdata:', str(x[variablenames[0]]), str(x[variablenames[1]]), str(x[variablenames[2]])
+
         if str(x[variablenames[0]]) not in xcategories:
             xcategories.append(str(x[variablenames[0]]))
         if str(x[variablenames[1]]) not in ycategories:
             ycategories.append(str(x[variablenames[1]]))
-        mydata.append([xcategories.index(str(x[variablenames[0]])),ycategories.index(str(x[variablenames[1]])),float(x[variablenames[2]])])
+        mydata.append([xcategories.index(str(x[variablenames[0]])),
+                      ycategories.index(str(x[variablenames[1]])),
+                      float(x[variablenames[2]])])
 
-    # print 'xcategories',str(xcategories)
-    # print 'ycategories',str(ycategories)
-    # print 'mydata', str(mydata)
+    consufion_matrix = {
+        'chart': {
+            'type': 'heatmap',
+            'marginTop': 40,
+            'marginBottom': 80,
+            'plotBorderWidth': 1,
+            },
+        'title': {'text': 'Confusion Matrix'},
+        'xAxis': {'categories': str(xcategories),
+                  'title': {'enabled': True,
+                  'style': {'fontWeight': 'normal'},
+                  'text': str(xtitle)}},
+        'yAxis': {'categories': str(ycategories),
+                  'title': {'enabled': True,
+                  'style': {'fontWeight': 'normal'},
+                  'text': str(ytitle)}},
+        'colorAxis': {'min': 0, 'minColor': '#FFFFFF',
+                      'maxColor': '#6699ff'},
+        'legend': {
+            'align': 'right',
+            'layout': 'vertical',
+            'margin': 0,
+            'verticalAlign': 'top',
+            'y': 25,
+            'symbolHeight': 280,
+            },
+        'tooltip': {'enabled': False},
+        'series': [{'borderWidth': 1, 'data': mydata,
+                   'dataLabels': {'enabled': True, 'color': '#000000'
+                   }}],
+        }
 
-    myresult="chart: {  type: 'heatmap', marginTop: 40, marginBottom: 80, plotBorderWidth: 1 },"
-    myresult += " title: { text: '" + title + "' },"
-    myresult += " xAxis: { categories: " + str(xcategories) + ",title: { enabled: true,  style: { fontWeight: 'normal'}, text: '" + xtitle +"'}},"
-    myresult += " yAxis: { categories: " + str(ycategories) + ",title: { enabled: true,  style: { fontWeight: 'normal'}, text: '" + ytitle +"'}},"
-    myresult += " colorAxis: { min: 0, minColor: '#FFFFFF', maxColor: Highcharts.getOptions().colors[0] },"
-    myresult += " legend: { align: 'right',layout: 'vertical', margin: 0, verticalAlign: 'top', y: 25, symbolHeight: 280 },"
-    myresult += "  tooltip: {formatter: function () {return '<b> (' + this.series.xAxis.categories[this.point.x] + ',' + this.series.yAxis.categories[this.point.y] +')=' +this.point.value + '</b>'; } },"
-    myresult += " series: [{  borderWidth: 1, data: "
-    myresult += str(mydata)
-    myresult += ", dataLabels: { enabled: true,color: '#000000'}}]"
-    return myresult
+    return consufion_matrix
+
 
 def main():
-	args = sys.argv[1:]
+    args = sys.argv[1:]
 
-	opts = getopts(args)
-		
-	if not opts or len(opts) < 5:
-		print("Usage:")
-		print(" -in Input")
-		print(" -t Title")
-		print(" -xt xTitle")
-		print(" -yt yTitle")
-		print(" -o Output")
-		return 0
-		
-	try:
-		inputFile = open(opts.get("-in"), "r")
-		inputData = inputFile.read()
-		inputJson = json.loads(inputData)
-		title = opts.get("-t")
-		xtitle = opts.get("-xt")
-		ytitle = opts.get("-yt")
-		results = inputJson['results']
-	except ValueError:
-		print("Input file should be:")
-		print("{")
-		print('  "results" : [ ... ]')
-		print("}")
-	
-	highcharts = []
-	for result in results:
-		highcharts.append(heatmap(result,title,xtitle,ytitle))
-	
-	outputJson = {"highcharts" : highcharts}
-	
-	outputFile = open(opts.get("-o"), "w")
-	outputFile.write(json.dumps(outputJson))
-	outputFile.close
+    opts = getopts(args)
 
-if __name__ == "__main__":
+    if not opts or len(opts) < 5:
+        print 'Usage:'
+        print ' -in Input'
+        print ' -t Title'
+        print ' -xt xTitle'
+        print ' -yt yTitle'
+        print ' -o Output'
+        return 0
+
+    try:
+        inputFile = open(opts.get('-in'), 'r')
+        inputData = inputFile.read()
+        inputJson = json.loads(inputData)
+        title = opts.get('-t')
+        xtitle = opts.get('-xt')
+        ytitle = opts.get('-yt')
+        previousResults = inputJson['results']
+    except ValueError:
+        print 'Input file should be:'
+        print '{'
+        print '  "results" : [ ... ]'
+        print '}'
+
+    results = []
+    for previousResult in previousResults:
+        result = {'result': [{'data': heatmap(previousResult, title,
+                  xtitle, ytitle),
+                  'type': 'application/vnd.highcharts+json'}]}
+        results.append(result)
+
+    outputJson = {'results': results}
+
+    outputFile = open(opts.get('-o'), 'w')
+    outputFile.write(json.dumps(outputJson))
+    outputFile.close
+
+
+if __name__ == '__main__':
     main()
